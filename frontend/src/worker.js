77a51bf3b85c87d016e8bcfb5dbdb5e6180b8fe2
myris3 <fritzolavmyrvang@hotmail.com>
2025-11-30 20:45:11 +0100

    turn of webgpu for now, and comment out message context

--- frontend/src/worker.js
+++ frontend/src/worker.js
@@ -15,16 +15,16 @@ self.addEventListener("message", (event) => {
 });
 
 const downloadModel = async (modelName) => {
-  pipe = await pipeline("text2text-generation", modelName, {device: 'webgpu'});
+  pipe = await pipeline("text2text-generation", modelName);
   self.postMessage({
     status: "ready",
   });
 };
 
-const generateResponse = async (content) => {
-  console.log("Generating response for:", content);
-  const prompt = messagesToPrompt(content);
-  const output = await pipe(prompt, { max_new_tokens: 200, temperature: 1.0, repetition_penalty: 2.0, no_repeat_ngram_size: 3 });
+const generateResponse = async (messages) => {
+  console.log("Generating response for:", messages);
+  //const prompt = messagesToPrompt(messages);
+  const output = await pipe(messages[messages.length-1].message , { max_new_tokens: 200, temperature: 1.0, repetition_penalty: 2.0, no_repeat_ngram_size: 3 });
   self.console.log("Model output:", output);
   self.postMessage({
     status: "response",
